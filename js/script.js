9d4c1dc6df66f0b335139f949ad6521b8f23e896
Alexandr Ryndin <alexandrryndin8@gmail.com>
2024-10-09 00:57:37 +0500

    Fixed burger, API still doesnsn't ready :)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
 // Burger part
 
-const burger = document.querySelector('.burger');
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-});
-
 document.getElementById("burger2").addEventListener("click", function() {
     document.querySelector(".navbar").classList.toggle("open")
-    })
+});
 
 // Math part
 
